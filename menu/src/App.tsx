da498d056e5915fa44c73921318f8438bd23d949
alejandro arvigo <alejandroarvigo@gmail.com>
2023-06-26 17:00:13 -0300

    Anduvo el compartir context con la api de react

--- menu/src/App.tsx
+++ menu/src/App.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { useGetShellContext } from "shell/store"
+import { useGetShellContext, ShellContextProvider } from "shell/store"
 
 import "./index.css";
 
-const App = () => {
+const Welcome = () => {
   const { logFunction } = useGetShellContext();
   console.log(logFunction);
+
   return (
     <div className="container">
       <button onClick={logFunction}>APRETA EL BOTON2</button>
     </div>
+  );
+}
+
+export default Welcome;
+
+
+const App = () => {
+  return (
+    <ShellContextProvider>
+      <Welcome />
+    </ShellContextProvider>
   );
 }
 ReactDOM.render(<App />, document.getElementById("app"));
